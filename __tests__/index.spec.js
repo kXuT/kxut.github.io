cdaeed43ec239c7cdcd58c147bbf1afcc24bdc41
kXuT <benjamin.rapt@viacesi.fr>
2024-06-13 10:00:02 +0200

    find hello world test

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,3 +1,18 @@
+/** @type {import('jest').Config} */
+const config = {
+  verbose: true,
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    html: '<html lang="zh-cmn-Hant"></html>',
+    url: 'https://jestjs.io/',
+    userAgent: 'Agent/007',
+  },
+};
+
+module.exports = config;
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 const fs = require('fs');
 const path = require('path');
 
@@ -5,11 +20,27 @@ const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
 jest.dontMock('fs');
 
 describe('$', function () {
+    let window, document;
+
     beforeEach(() => {
-        document.documentElement.innerHTML = html.toString();
+        const dom = new JSDOM(html, { url: 'https://jestjs.io/' });
+        window = dom.window;
+        document = window.document;
+        global.document = document;
+        global.window = window;
     });
-    afterEach(jest.resetModules);
+
+    afterEach(() => {
+        jest.resetModules();
+        delete global.document;
+        delete global.window;
+    });
+
     it('Cherche Hello World', function () {
-        window.onload(expect($('Hello World.')).toBeInTheDocument())
+        window.onload = () => {
+            const bodyText = document.body.textContent || document.body.innerText;
+            expect(bodyText).toContain('Hello World.');
+        };
+        window.onload();
     });
 });
